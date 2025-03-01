4fa28377c0fb17c392bc08630f57c44aed0909f4
mohabbaticicle <mohabbatali@iciclecorporation.com>
2025-03-02 03:11:09 +0600

    patient create component create

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,328 @@
-import React from "react";
+"use client";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  TextField,
+  Typography,
+  InputAdornment,
+  IconButton,
+  Divider,
+  Paper,
+  FormControlLabel,
+  Checkbox,
+  useMediaQuery,
+} from "@mui/material";
+import React, { useState } from "react";
+import logo from "@/assets/icons/logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { useTheme } from "@mui/material/styles";
 
 const RegisterPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const handleTogglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div>
-      <h1>This is Register Page !!!</h1>
-    </div>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        bgcolor: "#fafafa",
+        py: 4,
+      }}
+    >
+      <Container maxWidth="md">
+        <Paper
+          elevation={0}
+          sx={{
+            borderRadius: 3,
+            border: "1px solid",
+            borderColor: "divider",
+            p: { xs: 3, md: 4 },
+            bgcolor: "background.paper",
+            width: "100%",
+            maxWidth: 800,
+            mx: "auto",
+          }}
+        >
+          <Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                mb: 3,
+              }}
+            >
+              <Box sx={{ mb: 1 }}>
+                <Image src={logo} width={40} height={40} alt="logo" />
+              </Box>
+              <Typography
+                variant="h4"
+                component="h1"
+                fontWeight={700}
+                sx={{
+                  textAlign: "center",
+                  fontSize: { xs: "1.75rem", md: "1.7rem" },
+                }}
+              >
+                Patient Create
+              </Typography>
+              <Typography
+                variant="body1"
+                color="text.secondary"
+                sx={{ textAlign: "center", mt: 1 }}
+              >
+                Join our healthcare platform to manage your appointments and
+                medical records
+              </Typography>
+            </Box>
+          </Box>
+          <Grid container spacing={isMobile ? 0 : 6}>
+            <Grid item xs={12} md={6}>
+              <Box>
+                <Typography variant="subtitle1" fontWeight={600}>
+                  Account Information
+                </Typography>
+
+                <TextField
+                  fullWidth
+                  label="Full Name"
+                  variant="outlined"
+                  size="small"
+                  margin="normal"
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: "10px",
+                    },
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <PersonOutlineOutlinedIcon
+                          fontSize="small"
+                          color="action"
+                        />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+
+                <TextField
+                  fullWidth
+                  label="Email Address"
+                  variant="outlined"
+                  size="small"
+                  margin="normal"
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: "10px",
+                    },
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <EmailOutlinedIcon fontSize="small" color="action" />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+
+                <TextField
+                  fullWidth
+                  label="Password"
+                  variant="outlined"
+                  size="small"
+                  margin="normal"
+                  type={showPassword ? "text" : "password"}
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: "10px",
+                    },
+                  }}
+                  InputProps={{
+                    startAdornment: (
+                      <InputAdornment position="start">
+                        <LockOutlinedIcon fontSize="small" color="action" />
+                      </InputAdornment>
+                    ),
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleTogglePassword}
+                          edge="end"
+                          size="small"
+                        >
+                          {showPassword ? (
+                            <VisibilityOffOutlinedIcon fontSize="small" />
+                          ) : (
+                            <VisibilityOutlinedIcon fontSize="small" />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Box>
+            </Grid>
+
+            <Grid item xs={12} md={6}>
+              {isMobile && (
+                <Divider>
+                  <Typography variant="body2" color="text.secondary">
+                    Contact Details
+                  </Typography>
+                </Divider>
+              )}
+
+              {!isMobile && (
+                <Typography
+                  variant="subtitle1"
+                  fontWeight={600}
+                  // sx={{ mb: 3 }}
+                >
+                  Contact Details
+                </Typography>
+              )}
+
+              <TextField
+                fullWidth
+                label="Phone Number"
+                variant="outlined"
+                size="small"
+                margin="normal"
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    borderRadius: "10px",
+                  },
+                }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LocalPhoneOutlinedIcon fontSize="small" color="action" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+
+              <TextField
+                fullWidth
+                label="Address"
+                variant="outlined"
+                size="small"
+                margin="normal"
+                multiline
+                rows={3}
+                sx={{
+                  "& .MuiOutlinedInput-root": {
+                    borderRadius: "10px",
+                  },
+                }}
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment
+                      position="start"
+                      sx={{ alignSelf: "flex-start", mt: 1 }}
+                    >
+                      <HomeOutlinedIcon fontSize="small" color="action" />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </Grid>
+          </Grid>
+
+          <Box>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  color="primary"
+                  sx={{
+                    "& .MuiSvgIcon-root": {
+                      fontSize: 20,
+                      borderRadius: 1,
+                    },
+                  }}
+                />
+              }
+              label={
+                <Typography variant="body2">
+                  I agree to the{" "}
+                  <Link
+                    href="#"
+                    style={{
+                      color: theme.palette.primary.main,
+                      textDecoration: "none",
+                    }}
+                  >
+                    Terms of Service
+                  </Link>{" "}
+                  and{" "}
+                  <Link
+                    href="#"
+                    style={{
+                      color: theme.palette.primary.main,
+                      textDecoration: "none",
+                    }}
+                  >
+                    Privacy Policy
+                  </Link>
+                </Typography>
+              }
+            />
+          </Box>
+
+          <Button
+            variant="contained"
+            fullWidth
+            disableElevation
+            sx={{
+              // mt: 3,
+              py: 1,
+              borderRadius: 2,
+              textTransform: "none",
+              fontSize: "1rem",
+              fontWeight: 600,
+            }}
+          >
+            Create Patient
+          </Button>
+
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Typography variant="body2">
+              Already have an account?{" "}
+              <Link
+                href="/login"
+                style={{
+                  color: theme.palette.primary.main,
+                  fontWeight: 600,
+                  textDecoration: "none",
+                }}
+              >
+                Sign in here
+              </Link>
+            </Typography>
+          </Box>
+        </Paper>
+      </Container>
+    </Box>
   );
 };
 
